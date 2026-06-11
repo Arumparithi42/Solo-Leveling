c3d73040ccde686d30693d0ee0a8d60f17ed349a
Arumparithi42 <2024503542@student.annauniv.edu>
2026-06-11 21:51:55 +0530

    Added Weekly Quest

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import DailyQuest from "./DailyQuest"
+import WeeklyQuest from "./WeeklyQuest"
 import Practice from "./Practices";
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
    <>
    <DailyQuest></DailyQuest>
    {/* <Practice></Practice> */}
+    <WeeklyQuest></WeeklyQuest>
    </>
   )
 }
---
+++ src/WeeklyQuest.jsx
@@ -0,0 +1,11 @@
+import TodoTemplate from "./TodoTemplate";
+function DailyQuest(){
+    return(
+        <div className = "weeklyQuest">
+            <p>WEEKLY QUEST</p>
+        <TodoTemplate msg = "Article Writing"> </TodoTemplate>
+        
+        </div>
+    )
+}
+export default DailyQuest;
