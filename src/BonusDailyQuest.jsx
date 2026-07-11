db90e2c4e7da13081830a57ab89fe2a438cc19db
Arumparithi42 <2024503542@student.annauniv.edu>
2026-07-11 21:05:21 +0530

    Added progress tracking in Bonus Daily Quest

--- src/BonusDailyQuest.jsx
+++ src/BonusDailyQuest.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import TodoTemplate from "./TodoTemplate";
 function BonusDailyQuest(){
-    alert("You have Unlocked 'Bonus Daily Quest' by completing Daily Quest");
+    useEffect(()=>
+        alert("You have Unlocked 'Bonus Daily Quest' by completing Daily Quest"),[]
+    )
+
+    const [taskCompleted, setTaskCompleted] = useState(0);
+    const [progress, setProgress] = useState();
+    let msgs = [{msg : "DBMS Revision", id : 1, done : false},
+                {msg : "Speec", id : 2, done : false},
+                {msg : "Reading", id : 3, done : false},
+                {msg : "Type Writing", id : 4, done : false}];
+
+    let taskLength = msgs.length;
+    function handleDone(id){
+        let msg = msgs.filter((msg) => msg.id == id)
+        msg[0].done = true;
+        setTaskCompleted(taskCompleted + 1);
+    }
+    useEffect(()=>{
+        setProgress(taskCompleted/taskLength*100);
+    },[taskCompleted])
+    
     return(
         <div className = "bonusDailyQuest">
             <h2>BONUS DAILY QUEST</h2>
-        <TodoTemplate msg = "DBMS Revision"> </TodoTemplate>
-        <TodoTemplate msg = "Speech"> </TodoTemplate>
-        <TodoTemplate msg = "Reading"> </TodoTemplate>
-        <TodoTemplate msg = "Type Writing"> </TodoTemplate>
+        {msgs.map((msg) => <TodoTemplate key = {msg.id} id = {msg.id} msg = {msg.msg} onDone = {handleDone} ></TodoTemplate>)}
+        <p className="progress">Progress : {progress}%</p>
         </div>
     )
 }
